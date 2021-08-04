fd8bfeeb9884f2b71c7b300cf58f8353b2055816
Manoel-Augusto <manoelll@gmail.com>
2021-08-03 23:44:13 -0300

    frontweb criated/clean

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+const App=()=> {
+  return (
+    <div className="App">
+     <h1 className="bg-primary">Hello Word</h1>
+    </div>
+  );
+}
+
+export default App;
